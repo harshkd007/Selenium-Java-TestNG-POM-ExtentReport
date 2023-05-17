74bc628c99a1b7b463aa6696e2c939679626e257
harshkd007 <kadamharshavardhan042@gmail.com>
2023-05-17 21:37:53 +0530

    Removed the class

--- SelenumFramework/src/test/java/qa/test/TestCases.java
+++ SelenumFramework/src/test/java/qa/test/TestCases.java
@@ -3,7 +3,6 @@ package qa.test;
 import org.testng.annotations.Test;
 
 import baseTest.BaseTest;
-import pageEvents.CogmentoCRMEvents;
 import pageEvents.HomePagEvents;
 import pageEvents.LoginPageEvents;
 import utils.ElementFetch;
@@ -12,7 +11,6 @@ public class TestCases extends BaseTest{
 	ElementFetch elementFetch = new ElementFetch();
 	HomePagEvents homePage = new  HomePagEvents();
 	LoginPageEvents loginPage = new LoginPageEvents();
-	CogmentoCRMEvents cogmentoCRMEvents = new CogmentoCRMEvents();
 
 	@Test
 	public void sampleMethodForVerifingTitle() {
